65c0c29fb18484da96233a983564c10695bba9cc
eatcz <zfcstdio@163.com>
2024-04-12 19:46:14 +0800

    commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const ws = require('ws')
+const url = `wss://mempool.space/api/v1/ws`
+const wss = new ws(url)
+const app = express()
+
+let mempool = ''
+
+wss.onopen = () => {
+    // console.log('WebSocket connected');
+    wss.send(JSON.stringify({ action: 'init' }))
+    wss.send(JSON.stringify({
+        "action": "want",
+        "data": [
+            "blocks",
+            "stats",
+            "mempool-blocks",
+            "live-2h-chart"
+        ]
+    }))
+    wss.send(JSON.stringify({
+        "track-rbf-summary": true
+    }))
+    wss.send(JSON.stringify({
+        "track-mempool-block": 0
+    }))
+};
+
+wss.onmessage = (event) => {
+    // console.log('Received message:', event.data);
+    mempool = event.data
+    // 处理接收到的消息
+};
+
+
+app.get('/api/gas', (req, res) => {
+    res.send(mempool)
+})
+
+
+app.listen('8888', () => {
+    console.log('start')
+})
