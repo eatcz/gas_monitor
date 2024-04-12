1faec512caa8ebf9a0d4b68ec4421465ebe7d9c9
eatcz <zfcstdio@163.com>
2024-04-12 19:50:44 +0800

    update

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ wss.onmessage = (event) => {
     // 处理接收到的消息
 };
 
+app.get('/', (req, res) => {
+    res.send('自用BTC Gas监控,接口地址:/api/gas')
+})
 
 app.get('/api/gas', (req, res) => {
     res.send(mempool)
